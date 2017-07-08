27aadbe85d08da0fdfc2fc4cd9522c72f1cf4624
志毅 张 <cnoliverzhang@gmail.com>
2017-07-08 14:52:22 +0800

    icon&name added

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
     Widget w;
+    w.setWindowTitle(QObject::tr("Boogle"));
+    w.setWindowIcon(QIcon(":/images/images/Logo.png"));
     w.show();
 
     return a.exec();
