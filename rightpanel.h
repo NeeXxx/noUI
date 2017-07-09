bea157ee96117f214e9c88350aaba8d4103b63c1
志毅 张 <cnoliverzhang@gmail.com>
2017-07-09 20:16:47 +0800

    adjusted

---
+++ rightpanel.h
@@ -0,0 +1,15 @@
+#ifndef RIGHTPANEL_H
+#define RIGHTPANEL_H
+
+#include <QWidget>
+
+class rightPanel : public QWidget
+{
+    Q_OBJECT
+
+public:
+    rightPanel(QWidget *parent = 0);
+    ~rightPanel();
+};
+
+#endif // RIGHTPANEL_H
--- widget.cpp
+++ widget.cpp
@@ -1,11 +1,14 @@
 #include "widget.h"
+#include "rightpanel.h"
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
+    rightPanel *panel=new rightPanel;
     b=new  board;
-    QVBoxLayout* layout=new QVBoxLayout;
+    QHBoxLayout* layout=new QHBoxLayout;
     layout->addWidget(b);
+    layout->addWidget(panel);
     setLayout(layout);
     QPalette p;
     p.setBrush(this->backgroundRole(),QBrush(QColor(255,255,255)));
