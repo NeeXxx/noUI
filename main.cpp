20d2d8910f9737dcf0495e2a00fe0d3ed2c5acf8
CNOliverZhang <cnoliverzhang@gmail.com>
2017-07-13 01:41:22 +0800

    ui initialized

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
     Widget w;
-    w.setWindowTitle(QObject::tr("Boogle"));
+    w.setWindowTitle(QObject::tr("Gooble"));
     w.setWindowIcon(QIcon(":/images/images/Logo.png"));
     w.show();
 
